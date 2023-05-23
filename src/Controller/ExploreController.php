36e00f41ff9805daee7b5cb3520483ace3ce44cd
Junior <junior@wpj.cz>
2023-05-23 13:22:01 +0200

    GRAF HODNOCE/VLASTNOST - PRED ALG

--- src/Controller/ExploreController.php
+++ src/Controller/ExploreController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
+use App\Entity\User;
 use DateTime;
 
 
@@ -22,6 +23,8 @@ public function explore(PersistenceManagerRegistry $doctrine): Response
     if ($random) {
         $randomId = $random->getId();
         return $this->redirectToRoute('explore', ['id' => $randomId]);
+    }else{
+        return $this->redirectToRoute('/explore');
     }
 }
 
@@ -126,7 +129,6 @@ public function stats(ChartBuilderInterface $builder, PersistenceManagerRegistry
     $grafVek = $builder->createChart(Chart::TYPE_BAR);
     $grafVek->setData([
         'labels' => $names,
-        'left' => 'Likes',
         'datasets' => [
             [
                 'label' => 'Kdo je nejstarší?',
@@ -160,9 +162,84 @@ public function stats(ChartBuilderInterface $builder, PersistenceManagerRegistry
     ]);
 ///////////////////////////////////////////////////////////////
 
+    $repository = $doctrine->getRepository(Zkouska::class);
+    $frajer = $repository->count(['frajer'=> true]);
+    $smradoch = $repository->count(['smradoch'=> true]);
+    $chytrak = $repository->count(['chytrak'=> true]);
+    $slusnak = $repository->count(['slusnak'=> true]);
+    $getFrajer = $repository->findBy(['frajer'=> true]);
+    $getSmradoch = $repository->findBy(['smradoch'=> true]);
+    $getChytrak = $repository->findBy(['chytrak'=> true]);
+    $getSlusnak = $repository->findBy(['slusnak'=> true]);
+
+    $pocetFrajer = 0;
+    $pocetSmradoch = 0;
+    $pocetChytrak = 0;
+    $pocetSlusnak = 0;
+
+    foreach ($getFrajer as $row) {
+        $value = $row->getHodnoceni();
+        $pocetFrajer += $value;
+    }
+    foreach ($getSmradoch as $row) {
+        $value = $row->getHodnoceni();
+        $pocetSmradoch += $value;
+    }
+    foreach ($getChytrak as $row) {
+        $value = $row->getHodnoceni();
+        $pocetChytrak += $value;
+    }
+    foreach ($getSlusnak as $row) {
+        $value = $row->getHodnoceni();
+        $pocetSlusnak += $value;
+    }
+
+    $prumerFrajeri = $pocetFrajer / $frajer;
+    $prumerSmradosi = $pocetSmradoch / $smradoch;
+    $prumerChytraci = $pocetChytrak / $chytrak;
+    $prumerSlusnaci = $pocetSlusnak /$slusnak;
+
+
+
+
+    $grafKdo = $builder->createChart(Chart::TYPE_RADAR);
+    $grafKdo->setData([
+        'labels' => ['Frajeři', 'Smraďoši', 'Chytráci', 'Slušňáci'],
+        'datasets' => [
+            [
+                'label' => 'Průměrné hodnocení podle vlastností',
+                'backgroundColor' => [
+                    'rgba(255, 159, 64, 0.7)',
+
+
+            ],
+                'borderColor' => [
+                    'rgb(0, 0, 0)',
+            ],
+                'borderWidth' => 2,
+                'data' => [$prumerFrajeri,$prumerSmradosi,$prumerChytraci,$prumerSlusnaci]
+
+
+
+
+
+            ],
+        ],
+    ]);
+
+//$user = $this->getUser();
+//$username = $user->getUserIdentifier();
+
+
+//////////////////////////////////////////////////
+
+
+
     return $this->render('explore/stats.html.twig', [
         'grafik' => $grafik,
         'grafVek'=> $grafVek,
+        'grafKdo' => $grafKdo,
+
     ]);
 }
 
